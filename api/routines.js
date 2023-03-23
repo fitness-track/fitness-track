aca9b13334318d699a45552660d070dc430feec4
David Chaney <david.ray.chaney@gmail.com>
2023-03-23 14:23:06 -0500

    add get and post routines api functions

--- api/routines.js
+++ api/routines.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const {getRoutineById, getRoutinesWithoutActivities, getAllRoutines, getAllRoutinesByUser,
+       getPublicRoutinesByUser, getPublicRoutinesByActivity, createRoutine, updateRoutine, 
+       destroyRoutine} = require("../db")
 
 // GET /api/routines
-
+router.get("/routines", async(req, res)=>{
+  const request = await getAllRoutines();
+  res.send(request);
+})
 // POST /api/routines
-
+router.post("/routines", async(req, res)=>{
+  const request = await createRoutine(req.body);
+  res.send(request);
+})
 // PATCH /api/routines/:routineId
 
 // DELETE /api/routines/:routineId
